e58950c84ec4f90ea4e03c3434790dc0e52873cd
Vanush1998 <vanush.voskanyan@mail.ru>
2017-10-09 01:02:48 +0400

    dds

--- src/com/synisys/training/data/Gender.java
+++
@@ -1,5 +0,0 @@
-package com.synisys.training.data;
-
-public enum Gender {
-	Male,Female
-}
--- src/com/synisys/training/servlets/MyServlet.java
+++ src/com/synisys/training/servlets/MyServlet.java
@@ -1,24 +1,24 @@
 package com.synisys.training.servlets;
 
-import com.synisys.training.data.User;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 public class MyServlet
 		extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		response.addHeader("Access-Control-Allow-Origin", "*");
-		System.out.println(response.getStatus());
+		System.out.println(request.getMethod() + response.getStatus());
+
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		System.out.println("get");
+		response.addHeader("Access-Control-Allow-Origin", "*");
+		System.out.println(request.getMethod() + response.getStatus());
+
 	}
 }
