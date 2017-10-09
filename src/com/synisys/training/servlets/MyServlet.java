9fbe4789d4392f2ecb97da9647c85a9dbf45cdd9
Vanush1998 <vanush.voskanyan@mail.ru>
2017-10-09 16:18:34 +0400

    ddsd

--- src/com/synisys/training/servlets/MyServlet.java
+++ src/com/synisys/training/servlets/MyServlet.java
@@ -1,5 +1,7 @@
 package com.synisys.training.servlets;
 
+import org.apache.commons.io.IOUtils;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -8,17 +10,23 @@ import java.io.IOException;
 
 public class MyServlet
 		extends HttpServlet {
+
+	@Override protected void doHead(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		super.doHead(req, resp);
+	}
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		response.addHeader("Access-Control-Allow-Origin", "*");
-		System.out.println(request.getMethod() + response.getStatus());
+		System.out.println(request.getMethod() + " " + response.getStatus()+" ");
 
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		response.addHeader("Access-Control-Allow-Origin", "*");
-		System.out.println(request.getMethod() + response.getStatus());
-
+		System.out.println(request.getMethod() + " " + response.getStatus());
+		System.out.println(request.getParameter("a"));
 	}
 }
